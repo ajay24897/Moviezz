543011dc3121d63a0498e9dbcc7069fef23f0d9c
Ajay Chawada <ajay@Ajays-MacBook-Air.local>
2024-07-27 21:45:44 +0530

    feat : setup screens component and routing

---
+++ src/screens/homeScreen/searchMovie/searchMovie.js
@@ -0,0 +1,10 @@
+import {Text, View} from 'react-native';
+import React from 'react';
+
+export default function SearchMovie() {
+  return (
+    <View>
+      <Text>SearchMovie</Text>
+    </View>
+  );
+}
